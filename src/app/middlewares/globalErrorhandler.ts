3f2e693f8b5bb5b5e9b199d80dabd775d041f19b
Ekramul <mdekramulhassan168@gmail.com>
2024-06-02 20:55:39 +0600

    handelZodError added

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,16 +2,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = 500;
-  const message = err.message || 'Something went wrong!';
+import { ErrorRequestHandler } from 'express';
+import { TErrorSources } from '../interface/error';
+import { ZodError } from 'zod';
+import handleZodError from '../errors/handleZodError';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
