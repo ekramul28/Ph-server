1f865f657cda6912858579e7d6cc8efdeaa9cbc3
Ekramul <mdekramulhassan168@gmail.com>
2024-06-04 14:18:47 +0600

    unhandledRejection err and uncaughtException error handele added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import mongoose from 'mongoose';
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
+
+// eslint-disable-next-line no-unused-vars
+let service: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    service = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
   } catch (err) {
@@ -15,3 +20,18 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
