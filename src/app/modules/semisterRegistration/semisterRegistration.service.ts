3ec057b19902c4c266b2954a770a5a1559917814
Ekramul <mdekramulhassan168@gmail.com>
2024-06-09 11:21:14 +0600

    semesterRegistration added

--- src/app/modules/semisterRegistration/semisterRegistration.service.ts
+++ src/app/modules/semisterRegistration/semisterRegistration.service.ts
@@ -4,6 +4,9 @@ import { RegistrationStatus } from './semisterRegistration.constant';
 import { TSemesterRegistration } from './semisterRegistration.interface';
 import { SemesterRegistration } from './semisterRegistration.model';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { query } from 'express';
+import QueryBuilder from '../../builder/querybuilder';
+import mongoose from 'mongoose';
 
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
@@ -47,6 +50,101 @@ const createSemesterRegistrationIntoDB = async (
   return result;
 };
 
+const getAllSemesterRegistrationsFromDB = async (
+  query: Record<string, unknown>,
+) => {
+  const semesterRegistrationQuery = new QueryBuilder(
+    SemesterRegistration.find().populate('academicSemester'),
+    query,
+  )
+    .fields()
+    .sort()
+    .paginate()
+    .fields();
+  const result = await semesterRegistrationQuery.modelQuery;
+  return result;
+};
+const getSingleSemesterRegistrationsFromDB = async (id: string) => {
+  const result = await SemesterRegistration.findById(id);
+
+  return result;
+};
+
+const updateSemesterRegistrationIntoDB = async (
+  id: string,
+  payload: Partial<TSemesterRegistration>,
+) => {
+  const isSemesterRegistrationExists = await SemesterRegistration.findById(id);
+
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      `This semester is is not found ! `,
+    );
+  }
+
+  const currentSemesterStatus = isSemesterRegistrationExists?.status;
+  const registeredStatus = payload?.status;
+
+  if (currentSemesterStatus === RegistrationStatus.ENDED) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `this Semester is already ${currentSemesterStatus}`,
+    );
+  }
+
+  if (
+    currentSemesterStatus === RegistrationStatus.UPCOMING &&
+    registeredStatus === RegistrationStatus.ENDED
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `you can not directly chang status from ${currentSemesterStatus} to ${registeredStatus} `,
+    );
+  }
+
+  if (
+    currentSemesterStatus === RegistrationStatus.ONGOING &&
+    registeredStatus === RegistrationStatus.UPCOMING
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `you can not directly chang status from ${currentSemesterStatus} to ${registeredStatus} `,
+    );
+  }
+
+  const result = await SemesterRegistration.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return result;
+};
+
+const deleteSemesterRegistrationFromDB = async (id: string) => {
+  const isSemesterRegistrationExists = await SemesterRegistration.findById(id);
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'this registered semester is not found!',
+    );
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    // const deletedOfferedCourse=await offer
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const SemesterRegistrationService = {
   createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationsFromDB,
+  getSingleSemesterRegistrationsFromDB,
+  updateSemesterRegistrationIntoDB,
+  deleteSemesterRegistrationFromDB,
 };
