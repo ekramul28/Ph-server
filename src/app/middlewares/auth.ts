e6b1c5a02d3b3314f4fcdb386e99bb889797f660
Ekramul <mdekramulhassan168@gmail.com>
2024-06-16 22:54:19 +0600

    some code added

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+
+const auth = () => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.header?.authorization;
+    // checking if the token is missing
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    const { role, userId, iat } = decoded;
+
+    next();
+  });
+};
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,16 +2,46 @@ import httpStatus from 'http-status';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
-  const isUserExists = await User.findOne({ id: payload.id });
-  if (!isUserExists) {
+  const user = await User.isUserExistsByCustomId(payload.id);
+
+  if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
   }
-  const isDeleted = isUserExists?.isDeleted;
+  const isDeleted = user?.isDeleted;
   if (isDeleted) {
     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
   }
+  const userStatus = user?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
+  }
+
+  //checking if the password is correct
+  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+    throw new AppError(httpStatus.FORBIDDEN, 'password do not matched ! !');
+  }
+  //create token and send to the client
+
+  const jwtPayload = {
+    userId: user.id,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(
+    jwtPayload,
+
+    config.jwt_access_secret as string,
+    { expiresIn: '10d' },
+  );
+  return {
+    accessToken,
+    needsPasswordChange: user?.needsPasswordChange,
+  };
 };
 
 export const AuthService = {
